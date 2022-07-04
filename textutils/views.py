6041ec0381344aab40dd57c12272450e028a0ae2
Muhammad Anas <mrwhosetheboss365@gmail.com>
2022-07-04 17:59:59 +0500

    main

---
+++ textutils/views.py
@@ -0,0 +1,25 @@
+# I have created this file
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("new line")
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+
+def charcount(request):
+    return HttpResponse("charcount ")
+
+
