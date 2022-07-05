ae4b68ef3395b6f7ea5549b29581da716b73673f
Muhammad Anas <mrwhosetheboss365@gmail.com>
2022-07-05 15:41:11 +0500

    main

--- textutils/views.py
+++ textutils/views.py
@@ -1,19 +1,23 @@
 # I have created this file
 from django.http import HttpResponse
 from django.shortcuts import render
+import string
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 def analyze(request):
    # Get the text
    djtext = request.GET.get('text', 'default')
    removepunc = request.GET.get('removepunc', 'off')
-   print(removepunc)
-   print(djtext)
+   analysed = djtext.translate(djtext.maketrans('', '', string.punctuation))
+   params = {
+    "purpose" : "Removed Punctuations",
+    "analysed_text" : analysed,
+   }
 
    # Analyse the text
-   return HttpResponse(f"Your text: {djtext}")
+   return render(request, "analyse.html", params)
 
 
