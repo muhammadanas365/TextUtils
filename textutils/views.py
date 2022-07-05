929604aa558f16d3780990e56c973a49f25505ab
Muhammad Anas <mrwhosetheboss365@gmail.com>
2022-07-05 17:13:50 +0500

    main

--- textutils/views.py
+++ textutils/views.py
@@ -1,23 +1,40 @@
 # I have created this file
+
+# Import needed libraries
 from django.http import HttpResponse
 from django.shortcuts import render
 import string
 
 
-def index(request):
-    return render(request, "index.html")
+# The Home sweet Home Page
+def home(request):
+    return render(request, "home.html")
 
+
+# The Analyse Page LOGIC
 def analyze(request):
-   # Get the text
-   djtext = request.GET.get('text', 'default')
-   removepunc = request.GET.get('removepunc', 'off')
-   analysed = djtext.translate(djtext.maketrans('', '', string.punctuation))
-   params = {
+    # Get the text
+    djtext = request.GET.get('text', 'default')
+
+    # Get the filter info from the HTML
+    removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+
+    # Code the logic
+    if removepunc == "on":
+        analysed = djtext.translate(djtext.maketrans('', '', string.punctuation))
+    elif fullcaps == "on":
+        analysed = djtext.upper()
+    else:
+        analysed = djtext
+
+    # The params which are supposed to be passed on the render fucntion so the variables can be accessed in the HTML
+    params = {
     "purpose" : "Removed Punctuations",
     "analysed_text" : analysed,
-   }
+    }
 
-   # Analyse the text
-   return render(request, "analyse.html", params)
+    # Render the HTML template
+    return render(request, "analyse.html", params)
 
 
